42915d440551dffa8902404a73132aca6f55ca8c
DanGrund <hello@dangrund.com>
2016-12-12 22:18:43 -0700

    holy tits this is what we have after 5 hours

---
+++ lib/components/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TheApplication from './theapplication'
+
+export default class Main extends React.Component {
+
+
+  render () {
+    return (
+      <TheApplication/>
+    )
+  }
+}
